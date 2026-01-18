f9ee76669f4767faf01b2a95e5bc212f363a33c0
Rishabh <rishabh4402@gmail.com>
2026-01-18 05:15:18 -0800

    added Singleton patterns

---
+++ singleton/Main.java
@@ -0,0 +1,9 @@
+package singleton;
+
+import singleton.services.Singleton;
+
+public class Main {
+    public static void main(String[] args){
+        Singleton singleton = Singleton.getInstance();
+    }
+}
